c44ceced14827bff0d25a517882ee80d6f0bb030
David Thorn <david.thorn221278@googlemail.com>
2018-12-16 14:01:38 +0100

    changed tsconfig.json to ignore config folder

--- tests/chai.test.ts
+++
@@ -1,8 +0,0 @@
-import * as testData from '../src/Test'
-import { expect } from 'chai'
-
-describe('Test chai', () => {
-	it('should be true' , () => {
-		expect(testData.result).to.be.equal(true)
-	})
-})
